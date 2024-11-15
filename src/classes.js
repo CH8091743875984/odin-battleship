173f219d0aff6bbfe1a2c1f1279b2ae5d6b8c12d
CH8091743875984 <135175936+CH8091743875984@users.noreply.github.com>
2024-11-15 10:05:44 -0600

    Added gameboard class and tests for placement

--- src/classes.js
+++ src/classes.js
@@ -1,8 +1,9 @@
 export class Ship {
-  constructor(length, hitCount = 0, sunk = false) {
+  constructor(length) {
     this.length = length;
-    this.hitCount = hitCount;
-    this.sunk = sunk;
+    this.hitCount = 0;
+    this.sunk = false;
+    this.coordinates = [];
   }
 
   //only counts number of hits... relying on gameboard tracking to plot legal hits?
@@ -13,5 +14,69 @@ export class Ship {
 
   isSunk() {
     return this.length <= this.hitCount;
+  }
+}
+
+export class Gameboard {
+  constructor() {
+    this.grid = this.createGrid();
+    this.placements = [];
+  }
+
+  //create grid
+
+  createGrid() {
+    return Array.from({ length: 10 }, () => Array(10).fill(null));
+  }
+
+  getGridValue(x, y) {
+    if (x < 10 && y < 10) {
+      return this.grid[x][y];
+    }
+    return null;
+  }
+
+  isInBoundsSquare(x, y) {
+    return x < 10 && y < 10;
+  }
+
+  getOccupiedSquares() {
+    const coords = [];
+    for (let i = 0; i < this.placements.length; i++) {
+      coords.push(this.placements[i].coordinates);
+    }
+    //remember coords ends up being a nested array for each Ship; flatten to get all coords across all ships in one array
+    return coords.flat();
+  }
+
+  isSquareOccupied(x, y) {
+    return this.getOccupiedSquares().some(
+      (arr) => JSON.stringify(arr) === JSON.stringify([x, y])
+    );
+  }
+
+  placeShip(length, x, y, orientation) {
+    //validate coords?
+
+    const ship = new Ship(length);
+    let shipCoords = [];
+    for (let i = 0; i < length; i++) {
+      if (this.isSquareOccupied(x, y)) {
+        throw new Error("Square is occupied");
+      }
+      if (!this.isInBoundsSquare(x, y)) {
+        throw new Error("Square is out of bounds");
+      }
+
+      shipCoords.push([x, y]);
+      if (orientation === "horizontal") {
+        x++;
+      }
+      if (orientation === "vertical") {
+        y++;
+      }
+    }
+    ship.coordinates = shipCoords;
+    this.placements.push(ship);
   }
 }
--- src/classes.test.js
+++ src/classes.test.js
@@ -1,5 +1,6 @@
-import { Ship } from "./classes";
+import { Ship, Gameboard } from "./classes";
 
+//Ship tests
 test("create Ship object of length 4", () => {
   const destroyer = new Ship(4);
   expect(destroyer.length).toBe(4);
@@ -18,3 +19,81 @@ test("confirm sunk status remains False until 3/3 hits made on ship object of le
   submarine.hit();
   expect(submarine.sunk).toBe(true);
 });
+
+//Gameboard tests
+test("create Gameboard object with grid of 10x10, all values equal to null", () => {
+  const board = new Gameboard();
+  const expectedLayout = Array.from({ length: 10 }, () => Array(10).fill(null));
+  expect(board.grid).toEqual(expectedLayout);
+});
+
+test("empty board returns an empty array if no placements are made", () => {
+  const board = new Gameboard();
+  expect(board.getOccupiedSquares()).toEqual([]);
+});
+
+test("check for occupied square on the board returns False if no placements are made", () => {
+  const board = new Gameboard();
+  expect(board.isSquareOccupied(0, 0)).toBe(false);
+  expect(board.isSquareOccupied(9, 9)).toBe(false);
+});
+
+test("place a Ship of length 2 at coordinates 0,0 horizontally", () => {
+  const board = new Gameboard();
+  board.placeShip(2, 0, 0, "horizontal");
+  expect(board.placements[0].coordinates).toEqual([
+    [0, 0],
+    [1, 0],
+  ]);
+});
+
+test("place a Ship of length 4 at coordinates 1,1 vertically", () => {
+  const board = new Gameboard();
+  board.placeShip(4, 1, 1, "vertical");
+  expect(board.placements[0].coordinates).toEqual([
+    [1, 1],
+    [1, 2],
+    [1, 3],
+    [1, 4],
+  ]);
+});
+
+test("disallow placement of a Ship of length 5 at coordinates 6,7 horizontally (runs out of bounds)", () => {
+  //testing errors requires functions to be wrapped in a function
+  const board = new Gameboard();
+  expect(() => {
+    board.placeShip(5, 6, 7, "horizontal");
+  }).toThrow("Square is out of bounds");
+});
+
+test("disallow placement of a Ship of length 3 at coordinates 9,9 vertically (runs out of bounds)", () => {
+  const board = new Gameboard();
+  expect(() => {
+    board.placeShip(3, 9, 9, "vertical");
+  }).toThrow("Square is out of bounds");
+});
+
+test("disallow placement of Ship to overlap another", () => {
+  const board = new Gameboard();
+  board.placeShip(3, 4, 4, "horizontal");
+
+  expect(() => {
+    board.placeShip(5, 5, 3, "vertical");
+  }).toThrow("Square is occupied");
+});
+
+// test("place a Ship of length 2 at coordinates 0,0 vertically", () => {});
+
+// test("disallow a Ship of length 2 at coordinates 9,9 (out of bounds)", () => {});
+
+// test("disallow overlapping ship placement attempts");
+
+// test("record a missed shot");
+
+// test("record a hit shot and confirm the proper Ship has an updated hit count");
+
+// test("confirm that all ships have been sunk");
+
+// //Player tests
+// test("create human player with empty grid");
+// test("create computer player with empty grid");
