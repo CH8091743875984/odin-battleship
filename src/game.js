c75ebd63743abd45aa0e9d6647211358eeb81326
CH8091743875984 <135175936+CH8091743875984@users.noreply.github.com>
2024-11-17 09:55:22 -0600

    Added game class and initial functions and tests

---
+++ src/game.js
@@ -0,0 +1,50 @@
+import { Ship, Gameboard, Player } from "./classes";
+
+export class Game {
+  constructor() {
+    this.player1 = new Player("human");
+    this.player2 = new Player("computer");
+    this.defendingPlayer = this.player2;
+  }
+
+  setPiecesDemo() {
+    //maybe arg is the player object, if type is human await input
+    //if computer, follow AI strategy based on difficulty
+    //set player1 pieces
+    this.player1.board.placeShip(2, 0, 0, "horizontal");
+    this.player1.board.placeShip(3, 2, 2, "horizontal");
+    this.player1.board.placeShip(3, 9, 0, "vertical");
+    this.player1.board.placeShip(4, 3, 7, "horizontal");
+    this.player1.board.placeShip(5, 2, 9, "horizontal");
+
+    //set player2 pieces
+    this.player2.board.placeShip(2, 5, 0, "vertical");
+    this.player2.board.placeShip(3, 6, 1, "horizontal");
+    this.player2.board.placeShip(3, 0, 6, "vertical");
+    this.player2.board.placeShip(4, 4, 8, "horizontal");
+    this.player2.board.placeShip(5, 0, 9, "horizontal");
+  }
+
+  changeDefendingPlayer() {
+    //change who is receiving attacks
+    this.activePlayer =
+      this.activePlayer === this.player1 ? this.player2 : this.player1;
+  }
+
+  checkGameOver() {
+    return (
+      this.player1.board.checkGameOver() || this.player2.board.checkGameOver()
+    );
+  }
+
+  playRound(x, y) {
+    //are we going to rely on event listeners to prevent clicks?
+    //, else how to do retries?
+    this.defendingPlayer.board.receiveAttack(x, y);
+
+    if (this.checkGameOver()) {
+      alert("game is over!");
+    }
+    this.changeDefendingPlayer();
+  }
+}
---
+++ src/game.test.js
@@ -0,0 +1,80 @@
+import { Game } from "./game";
+
+test("Initialize game, confirm players 1 and 2 created", () => {
+  const game = new Game();
+
+  expect(game.player1.playerType).toBe("human");
+  expect(game.player2.playerType).toBe("computer");
+});
+
+test("Initialize game, confirm demo placements exist in each player", () => {
+  const testGame = new Game();
+  testGame.setPiecesDemo();
+
+  const player1Placements = testGame.player1.board.getOccupiedSquares();
+
+  // this.player1.board.placeShip(2, 0, 0, "horizontal");
+  // this.player1.board.placeShip(3, 2, 2, "horizontal");
+  // this.player1.board.placeShip(3, 9, 0, "vertical");
+  // this.player1.board.placeShip(4, 3, 7, "horizontal");
+  // this.player1.board.placeShip(5, 2, 9, "horizontal");
+
+  const expectedOccupiedSquares1 = [
+    [0, 0],
+    [1, 0],
+    //next
+    [2, 2],
+    [3, 2],
+    [4, 2],
+    //next
+    [9, 0],
+    [9, 1],
+    [9, 2],
+    //next
+    [3, 7],
+    [4, 7],
+    [5, 7],
+    [6, 7],
+    //next
+    [2, 9],
+    [3, 9],
+    [4, 9],
+    [5, 9],
+    [6, 9],
+  ];
+  expect(player1Placements).toEqual(expectedOccupiedSquares1);
+
+  // this.player2.board.placeShip(2, 5, 0, "vertical");
+  // this.player2.board.placeShip(3, 6, 1, "horizontal");
+  // this.player2.board.placeShip(3, 0, 6, "vertical");
+  // this.player2.board.placeShip(4, 4, 8, "horizontal");
+  // this.player2.board.placeShip(5, 0, 9, "horizontal");
+
+  const player2Placements = testGame.player2.board.getOccupiedSquares();
+
+  const expectedOccupiedSquares2 = [
+    [5, 0],
+    [5, 1],
+    //next
+    [6, 1],
+    [7, 1],
+    [8, 1],
+    //next
+    [0, 6],
+    [0, 7],
+    [0, 8],
+    //next
+    [4, 8],
+    [5, 8],
+    [6, 8],
+    [7, 8],
+    //next
+    [0, 9],
+    [1, 9],
+    [2, 9],
+    [3, 9],
+    [4, 9],
+  ];
+
+  expect(player2Placements).toEqual(expectedOccupiedSquares2);
+});
